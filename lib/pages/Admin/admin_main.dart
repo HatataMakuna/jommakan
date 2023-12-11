34178fb6d2908263a4fb1891da88ae39f1cd063e
ii887522 <ii887522@gmail.com>
2023-12-11 12:06:21 +0800

    User side only portrait mode, Admin side no restriction

--- lib/pages/Admin/admin_main.dart
+++ lib/pages/Admin/admin_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:jom_makan/pages/Admin/router/router_info.dart';
 import 'package:responsive_framework/responsive_wrapper.dart';
 
@@ -7,17 +8,34 @@ void main() {
   runApp(const AdminMainPage());
 }
 
-class AdminMainPage extends StatelessWidget {
+class AdminMainPage extends StatefulWidget {
   const AdminMainPage({super.key});
+
+  @override
+  State<AdminMainPage> createState() => _AdminMainPageState();
+}
+
+class _AdminMainPageState extends State<AdminMainPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+  }
 
   @override
   Widget build(BuildContext context) {
     ThemeData themeData = ThemeData(
         primarySwatch: Colors.blue,
         tabBarTheme: Theme.of(context).tabBarTheme.copyWith(
-          labelColor: Colors.black26,
-          labelStyle: const TextStyle(color: Colors.black26, fontSize: 14),
-        ));
+              labelColor: Colors.black26,
+              labelStyle: const TextStyle(color: Colors.black26, fontSize: 14),
+            ));
 
     return MaterialApp.router(
       theme: themeData,
@@ -25,17 +43,18 @@ class AdminMainPage extends StatelessWidget {
       routerConfig: AdminRouter().goRouter,
       builder: (routerContext, child) {
         return ResponsiveWrapper.builder(
-            child,
-            maxWidth: 2460,
-            minWidth: 280,
-            defaultScale: true,
-            breakpoints: [
-              const ResponsiveBreakpoint.resize(280, name: MOBILE),
-              const ResponsiveBreakpoint.autoScale(912, name: TABLET),
-              const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
-              const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
-            ],
-            background: Container(color: const Color(0xFFF5F5F5)));
+          child,
+          maxWidth: 2460,
+          minWidth: 280,
+          defaultScale: true,
+          breakpoints: [
+            const ResponsiveBreakpoint.resize(280, name: MOBILE),
+            const ResponsiveBreakpoint.autoScale(912, name: TABLET),
+            const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+            const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+          ],
+          background: Container(color: const Color(0xFFF5F5F5)),
+        );
       },
     );
   }
